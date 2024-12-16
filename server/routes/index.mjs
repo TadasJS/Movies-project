910b6461686bc79c1be975fee4f4251a7a61fc1a
Tadas <tadas.x@gmail.com>
2024-12-17 01:26:41 +0200

    create user back

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -4,6 +4,7 @@ import { dbtest } from "./dbTestRoutes.mjs";
 import { movies } from "./moviesRoutes.mjs";
 import { tvshows } from './tvShowsRoutes.mjs';
 import { login } from './loginRoutes.mjs';
+import { users } from './userRoutes.mjs';
 
 const api = express.Router()
 
@@ -11,6 +12,7 @@ api.use('/dbtest', dbtest)
 api.use('/movies',movies)
 api.use('/tvshows',tvshows)
 api.use('/login',login)
+api.use('/users',users)
 
 api.get('/', (req, res) => {
     res.status(200).json({status:'ok', msg:'Wrong address. API page is being created. Try another address'})
